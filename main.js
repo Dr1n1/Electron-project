25905699313f474ad8c88d3a22eb9ecc69572520
Dr1n1 <krasniqi.drin97@gmail.com>
2023-05-09 11:01:34 +0200

    final version

--- main.js
+++ main.js
@@ -1,183 +1,138 @@
 const path = require('path')
-const { app, BrowserWindow, Menu } = require('electron');
+const os = require('os')
+const fs = require('fs')
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+
+const resizeImg = require('resize-img')
+
+process.env.NODE_ENV = "production"
 
 const isDev = process.env.NODE_ENV !== 'development';
 const isMac = process.platform === 'darwin';
 
+let mainWindow;
 
 function createMainWindow() {
-    const mainWindow = new BrowserWindow({
-        title: 'Image Resizer',
-        width:  1000,
-        height: 600,
-        // resizable: isDev,
-    });
-
-    if (isDev) {
-        mainWindow.webContents.openDevTools();
+  mainWindow = new BrowserWindow({
+    title: 'Image Resizer',
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
     }
+  });
 
-    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
 function createAboutWindow() {
-    aboutWindow = new BrowserWindow({
-        width: 300,
-        height: 300,
-        title: 'About Electron',
-        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-    });
+  aboutWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: 'About Electron',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+  });
 
-    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+  aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
 }
 
 app.whenReady().then(() => {
-    createMainWindow();
+  createMainWindow();
 
-    const mainMenu = Menu.buildFromTemplate(menu);
-    Menu.setApplicationMenu(mainMenu)
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu)
+  mainWindow.on('closed', () => (mainWindow = null));
 });
 
+
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createMainWindow()
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
 })
 const menu = [
-    ...(isMac
-      ? [
+  ...(isMac
+    ? [
+      {
+        label: app.name,
+        submenu: [
           {
-            label: app.name,
-            submenu: [
-              {
-                label: 'About',
-                click: createAboutWindow,
-              },
-            ],
+            label: 'About',
+            click: createAboutWindow,
           },
-        ]
-      : []),
-    {
-      role: 'fileMenu',
-    },
-    ...(!isMac
-      ? [
+        ],
+      },
+    ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+      {
+        label: 'Help',
+        submenu: [
           {
-            label: 'Help',
-            submenu: [
-              {
-                label: 'About',
-                click: createAboutWindow,
-              },
-            ],
+            label: 'About',
+            click: createAboutWindow,
           },
-        ]
-      : []),
-    // {
-    //   label: 'File',
-    //   submenu: [
-    //     {
-    //       label: 'Quit',
-    //       click: () => app.quit(),
-    //       accelerator: 'CmdOrCtrl+W',
-    //     },
-    //   ],
-    // },
-    ...(isDev
-      ? [
-          {
-            label: 'Developer',
-            submenu: [
-              { role: 'reload' },
-              { role: 'forcereload' },
-              { type: 'separator' },
-              { role: 'toggledevtools' },
-            ],
-          },
-        ]
-      : []),
-  ];
+        ],
+      },
+    ]
+    : []),
 
-// const menu = [
-//     ...(isMac
-//         ? [
-//             {
-//                 label: app.name,
-//                 submenu: [
-//                     {
-//                         label: 'About',
-//                         click: createAboutWindow,
-//                     },
-//                 ],
-//             },
-//         ]
-//         : []),
-//     {
-//         role: 'fileMenu',
-//     },
-//     ...(isMac ? [{
-//         label: 'Help',
-//         submenu: [{
-//             label: 'About',
-//             click: createAboutWindow
-//         }]
-//     }] : [])
-// ];
+  ...(isDev
+    ? [
+      {
+        label: 'Developer',
+        submenu: [
+          { role: 'reload' },
+          { role: 'forcereload' },
+          { type: 'separator' },
+          { role: 'toggledevtools' },
+        ],
+      },
+    ]
+    : []),
+];
 
-// ...(isMac
-//     ? [
-//         {
-//             label: app.name,
-//             submenu: [
-//                 {
-//                     label: 'About',
-//                     click: createAboutWindow,
-//                 },
-//             ],
-//         },
-//     ]
-//     : []),
-// {
-//     role: 'fileMenu',
-// },
-// ...(!isMac
-//     ? [
-//         {
-//             label: app.name,
-//             submenu: [
-//                 {
-//                     label: 'About',
-//                 },
-//             ],
-//         },
-//     ]
-//     : []),
-// {
-//     role: 'fileMenu'
-// },
-// ...(!isMac ? [{
-//     label: 'Help',
-//     submenu: [{
-//         label: 'About'
-//     }]
-// }] : [])
+ipcMain.on('image:resize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imgresizer')
+  resizeImage(options);
+});
 
-// ...(isDev
-//   ? [
-//       {
-//         label: 'Developer',
-//         submenu: [
-//           { role: 'reload' },
-//           { role: 'forcereload' },
-//           { type: 'separator' },
-//           { role: 'toggledevtools' },
-//         ],
-//       },
-//     ]
-//   : []),
+async function resizeImage({ imgPath, width, height, dest }) {
+  try {
+    const newPath = await resizeImg(fs.readFileSync(imgPath), {
+      width: +width,
+      height: +height,
+    });
+
+    const filename = path.basename(imgPath);
+
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+    fs.writeFileSync(path.join(dest, filename), newPath);
+
+    mainWindow.webContents.send('image:done')
+
+    shell.openPath(dest);
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
 app.on('windwos-all-closed', () => {
 
-    if (!isMac) {
-        app.quit()
-    }
+  if (!isMac) {
+    app.quit()
+  }
 })
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -5,22 +5,82 @@ const filename = document.querySelector('#filename');
 const heightInput = document.querySelector('#height');
 const widthInput = document.querySelector('#width');
 
+
 function loadImage(e) {
   const file = e.target.files[0];
 
   if (!isFileImage(file)) {
-    console.log('Wrong format. Please select a valid image file.');
+    alertError('Wrong format. Please select a valid image file.');
     return;
   }
   // console.log('Success');
 
-  form.style.display= 'block';
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  }
+
+  form.style.display = 'block';
   filename.innerText = file.name;
+  outputPath.innerText = path.join(os.homedir(), 'electronproject')
+}
+function sendImage(e) {
+  e.preventDefault();
+  const width = widthInput.value;
+  const height = heightInput.value;
+  const imgPath = img.files[0].path;
+  
+
+  if (!img.files[0]) {
+    alertError('Upload image');
+    return
+  }
+  if (width === '' || height === '') {
+    alertError("Please fill in a height and width");
+    return
+  }
+  
+  ipcRenderer.send('image:resize', {
+    imgPath,
+    width,
+    height,
+  })
 }
 
+ipcRenderer.on('image:done', () => {
+  alertSuccess(`Image size to ${widthInput.value} x ${heightInput.value}`);
+})
 function isFileImage(file) {
   const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg'];
   return file && acceptedImageTypes.includes(file.type);
 }
 
+function alertError(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: 'red',
+      color: 'white',
+      textAlign: 'center'
+    }
+  })
+}
+function alertSuccess(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: 'green',
+      color: 'white',
+      textAlign: 'center'
+    }
+  })
+}
+
 img.addEventListener('change', loadImage);
+form.addEventListener('submit', sendImage)
